f528b86d2a542a90caabfac3946f874ff6e5781c
ArchiAiLAB <141567987+ArchiAiLAB@users.noreply.github.com>
2026-01-30 08:45:58 +0800

    Delete yolo-det_predict-edu.py

--- yolo-det_predict-edu.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("user_best.pt")  
-results = model(
-    source = r'C:\Users\User\Documents\...\test\images',
-    imgsz = 640,
-    save = True,
-    save_txt = True    
-    )  
